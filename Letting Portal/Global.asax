a94491f79467746a06df20a50158c29afea1ad01
IonaShanks <ihs59035@gmail.com>
2020-05-24 12:03:22 +0100

    Add project files.

---
+++ Letting Portal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Letting_Portal.MvcApplication" Language="C#" %>
---
+++ Letting Portal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Letting_Portal.Startup))]
+namespace Letting_Portal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
